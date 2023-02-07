11c95b515d5af1c1f02cb304ce38f92f566eb559
Nicolas Albuquerque <contato.nicolasalbuquerque@gmail.com>
2023-02-07 11:54:06 -0300

    hello world

---
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+			System.out.println("hello World.");
+			System.out.println("turma 61 é top");
+			
+
+	}
+
+}
