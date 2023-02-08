01268f0dc862d4e63ad76586f0d294b8f5488d29
Nicolas Albuquerque <contato.nicolasalbuquerque@gmail.com>
2023-02-08 16:45:05 -0300

    Correção.

--- aula_01/src/aula_01/DiferençaDoProduto.java
+++ aula_01/src/aula_01/DiferençaDoProduto.java
@@ -6,18 +6,18 @@ public class DiferençaDoProduto {
 
 	public static void main(String[] args) {
 	Scanner sc =new Scanner (System.in);
-	int n1,n2,n3,n4;
-	System.err.println("Digite o primeiro número: ");
-	n1=sc.nextInt();
+	float n1,n2,n3,n4;
+	System.out.println("Digite o primeiro número: ");
+	n1=sc.nextFloat();
 	System.out.println("Digite o segundo número: ");
-	n2=sc.nextInt();
+	n2=sc.nextFloat();
 	System.out.println("Digite o terceiro número: ");
-	n3=sc.nextInt();
+	n3=sc.nextFloat();
 	System.out.println("Digiite o quarto número: ");
-	n4=sc.nextInt();
-	int calculo= (n1 * n2)- (n3 * n4);
+	n4=sc.nextFloat();
+	float calculo= (n1 * n2)- (n3 * n4);
 	
-	System.out.println("A diferença é : "+ calculo);
+	System.out.println("A diferença é : "+ (int)calculo);
 	
 	}
 
